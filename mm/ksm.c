fad74a3008396770b54cabfcb46427e5ad238594
UpInTheAir <tyson_cj@hotmail.com>
2014-11-20 21:04:59 +0700

    mm/ksm: engage deferred timer by default [faux123]

--- mm/ksm.c
+++ mm/ksm.c
@@ -233,7 +233,7 @@ static int ksm_nr_node_ids = 1;
 #endif
 
 /* Boolean to indicate whether to use deferred timer or not */
-static bool use_deferred_timer;
+static bool use_deferred_timer = true;
 
 #define KSM_RUN_STOP	0
 #define KSM_RUN_MERGE	1
