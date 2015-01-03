a15a0588c9743d159d8ab4d26501721a3025b8af
UpInTheAir <upintheair.xda@gmail.com>
2015-01-03 15:37:21 +0700

    drivers/staging: configurable android log buffer sizes [Bhargav Upperla]
    03/1/2015: Adapted for Samsung Galaxy Note Edge Exynos 5433 3.10.9+ by UpInTheAir
    
    Purpose is reduce the buffer size accordingly and observe savings in the total kernel static size, which is useful for memory constrained devices.
    
    Change-Id: I926ab85213c82924ec483327d5aa248372efc604
    Signed-off-by: Bhargav Upperla <bhargavuln@codeaurora.org>

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -35,6 +35,10 @@
 #include <asm/ioctls.h>
 #include <linux/sec_debug.h>
 
+#ifndef CONFIG_LOGCAT_SIZE
+#define CONFIG_LOGCAT_SIZE 256
+#endif
+
 /**
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
  * @buffer:	The actual ring buffer
@@ -879,24 +883,24 @@ static int __init logger_init(void)
 {
 	int ret;
 
-	ret = create_log(LOGGER_LOG_MAIN, 2048*1024);
+	ret = create_log(LOGGER_LOG_MAIN, CONFIG_LOGCAT_SIZE*1024);
 	if (unlikely(ret))
 		goto out;
 
-	ret = create_log(LOGGER_LOG_EVENTS, 256*1024);
+	ret = create_log(LOGGER_LOG_EVENTS, CONFIG_LOGCAT_SIZE*1024);
 	if (unlikely(ret))
 		goto out;
 
-	ret = create_log(LOGGER_LOG_RADIO, 2048*1024);
+	ret = create_log(LOGGER_LOG_RADIO, CONFIG_LOGCAT_SIZE*1024);
 	if (unlikely(ret))
 		goto out;
 
-	ret = create_log(LOGGER_LOG_SYSTEM, 256*1024);
+	ret = create_log(LOGGER_LOG_SYSTEM, CONFIG_LOGCAT_SIZE*1024);
 	if (unlikely(ret))
 		goto out;
 
 #if !defined(CONFIG_SAMSUNG_PRODUCT_SHIP)
-	ret = create_log(LOGGER_LOG_SF, 256*1024);
+	ret = create_log(LOGGER_LOG_SF, CONFIG_LOGCAT_SIZE*1024);
 	if (unlikely(ret))
 		goto out;
 #endif
