e21f6b5c81663f5b91ec910a3d5d29f8bf12ef5b
halaszk <halaszk@deathlord.eu>
2015-02-27 14:11:08 +0100

    Revert " selinux: put the mmap() DAC controls before the MAC controls"
    This reverts commit cb7364752caec019db59f5418bb3c82e87be9dd5.

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3198,7 +3198,8 @@ error:
 
 static int selinux_mmap_addr(unsigned long addr)
 {
-	int rc;	
+	int rc = 0;
+	u32 sid = current_sid();
 
 	/*
 	 * notice that we are intentionally putting the SELinux check before
@@ -3206,18 +3207,15 @@ static int selinux_mmap_addr(unsigned long addr)
 	 * at bad behaviour/exploit that we always want to get the AVC, even
 	 * if DAC would have also denied the operation.
 	 */
-	/* do DAC check on address space usage */
-	rc = cap_mmap_addr(addr);
-	if (rc)
-		return rc;
-
 	if (addr < CONFIG_LSM_MMAP_MIN_ADDR) {
-		u32 sid = current_sid();
 		rc = avc_has_perm(sid, sid, SECCLASS_MEMPROTECT,
 				  MEMPROTECT__MMAP_ZERO, NULL);
+		if (rc)
+			return rc;
 	}
 
-	return rc;
+	/* do DAC check on address space usage */
+	return cap_mmap_addr(addr);
 }
 
 static int selinux_mmap_file(struct file *file, unsigned long reqprot,
