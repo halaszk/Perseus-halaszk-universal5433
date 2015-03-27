ec676bf18d3e343d670016ddeedc39067fca40f4
halaszk <halaszk@deathlord.eu>
2015-03-27 12:47:32 +0100

    bcmdhd: reduce rx wakelock timeout via sysfs node
    /sys/module/bcmdhd/parameters/wl_divide

--- drivers/net/wireless/bcmdhd4358/dhd_linux.c
+++ drivers/net/wireless/bcmdhd4358/dhd_linux.c
@@ -192,6 +192,11 @@ extern bool ap_fw_loaded;
 
 #include <wl_android.h>
 
+#include <linux/moduleparam.h>
+
+static int wl_divide = 1;
+module_param(wl_divide, int, 0644);
+
 /* Maximum STA per radio */
 #define DHD_MAX_STA     32
 
@@ -8912,10 +8917,10 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 #ifdef CONFIG_HAS_WAKELOCK
 		if (dhd->wakelock_rx_timeout_enable)
 			wake_lock_timeout(&dhd->wl_rxwake,
-				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable)/4);
+				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable)/wl_divide);
 		if (dhd->wakelock_ctrl_timeout_enable)
 			wake_lock_timeout(&dhd->wl_ctrlwake,
-				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable)/4);
+				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable)/wl_divide);
 #endif
 		dhd->wakelock_rx_timeout_enable = 0;
 		dhd->wakelock_ctrl_timeout_enable = 0;
