3f15a937e9f19d6137c70324c79040ae173b12ae
halaszk <halaszk@deathlord.eu>
2015-04-19 14:47:03 +0200

    Linux 3.10.43

--- drivers/usb/core/driver.c
+++ drivers/usb/core/driver.c
@@ -1759,8 +1759,10 @@ int usb_runtime_suspend(struct device *dev)
 
 	/* The PM core reacts badly unless the return code is 0,
 	 * -EAGAIN, or -EBUSY, so always return -EBUSY on an error.
+	(except for root hubs, because they don't suspend through
+	an upstream port like other USB devices).
 	 */
-	if (status != 0)
+	if (status != 0 && udev->parent)
 		return -EBUSY;
 
 	return status;
