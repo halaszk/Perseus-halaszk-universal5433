43522d2a55fc56b3fc068f9095bbf2072864e9ac
halaszk <halaszk@deathlord.eu>
2015-05-09 12:08:19 +0200

    hmp: sched: Clean up hmp_up_threshold checks into a utility fn
    In anticipation of modifying the up_threshold handling, make all
    instances use the same utility fn to check if a task is eligible
    for up-migration. This also removes the previous difference in
    threshold comparison where up-migration used '!<threshold' and
    idle pull used '>threshold' to decide up-migration eligibility.
    Make them both use '!<threshold' instead for consistency, although
    this is unlikely to change any results.
    
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7237,6 +7237,14 @@ static void nohz_idle_balance(int this_cpu, enum cpu_idle_type idle) { }
 #endif
 
 #ifdef CONFIG_SCHED_HMP
+static unsigned int hmp_task_eligible_for_up_migration(struct sched_entity *se)
+{
+	/* below hmp_up_threshold, never eligible */
+	if (se->avg.load_avg_ratio < hmp_up_threshold)
+		return 0;
+	return 1;
+}
+
 /* Check if task should migrate to a faster cpu */
 static unsigned int hmp_up_migration(int cpu, int *target_cpu, struct sched_entity *se)
 {
@@ -7260,7 +7268,7 @@ static unsigned int hmp_up_migration(int cpu, int *target_cpu, struct sched_enti
 		else
 			up_threshold = hmp_up_threshold;
 
-		if (se->avg.load_avg_ratio < up_threshold)
+		if (!hmp_task_eligible_for_up_migration(se))
 			return 0;
 	}
 
@@ -7744,7 +7752,7 @@ static unsigned int hmp_idle_pull(int this_cpu)
 		else
 			up_threshold = hmp_up_threshold;
 
-		if (hmp_boost() || curr->avg.load_avg_ratio > up_threshold)
+		if (hmp_boost() || hmp_task_eligible_for_up_migration(curr))
 			if (curr->avg.load_avg_ratio > ratio) {
 				p = task_of(curr);
 				target = rq;
