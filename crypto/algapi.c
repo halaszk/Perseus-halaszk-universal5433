81c7818ddc2372e1fd20766daad27a12a6373ffb
UpInTheAir <upintheair.xda@gmail.com>
2015-06-29 19:25:47 +0700

    Linux 3.10.67

--- crypto/algapi.c
+++ crypto/algapi.c
@@ -560,8 +560,8 @@ struct crypto_template *crypto_lookup_template(const char *name)
 		return ERR_PTR(-EACCES);
 	}
 #endif
-	return try_then_request_module(__crypto_lookup_template(name), "%s",
-				       name);
+	return try_then_request_module(__crypto_lookup_template(name),
+			       "crypto-%s", name);
 }
 EXPORT_SYMBOL_GPL(crypto_lookup_template);
 
