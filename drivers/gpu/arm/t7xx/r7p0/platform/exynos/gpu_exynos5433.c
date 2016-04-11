55e5fac75c4af7d205867959492dbc6aa4597fd8
halaszk <halaszk@deathlord.eu>
2016-04-11 13:35:04 +0200

    GPU-MALI-T760: Added GPU clock control interface

--- drivers/gpu/arm/t7xx/r7p0/platform/exynos/gpu_exynos5433.c
+++ drivers/gpu/arm/t7xx/r7p0/platform/exynos/gpu_exynos5433.c
@@ -66,8 +66,9 @@ static int available_max_clock[] = {GPU_L2, GPU_L2, GPU_L0, GPU_L0, GPU_L0};
 
 static gpu_attribute gpu_config_attributes[] = {
 	{GPU_MAX_CLOCK, 700},
-	{GPU_MAX_CLOCK_LIMIT, 600},
+	{GPU_MAX_CLOCK_LIMIT, 700},
 	{GPU_MIN_CLOCK, 100},
+	{GPU_MIN_CLOCK_LIMIT, 100},
 	{GPU_DVFS_START_CLOCK, 266},
 	{GPU_DVFS_BL_CONFIG_CLOCK, 266},
 	{GPU_GOVERNOR_TYPE, G3D_DVFS_GOVERNOR_INTERACTIVE},
